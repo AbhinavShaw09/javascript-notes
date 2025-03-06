d590e7bef15ce14db7afcee734fefa5d31caf87c
Abhinav <shawabhinav93@gmail.com>
2025-03-06 23:25:41 +0530

    started with js fundamentals 🔥

---
+++ fundamentals/index.js
@@ -0,0 +1,10 @@
+console.log("Statement ends with a semicolon") // This is a statement
+console.log("Semicolon ommited before statement")
+
+// alert("Hello")
+// [1, 2].forEach(alert);
+/*
+    Javascript treats this as one line , it does not insert the semicolon before [] 
+    so the above code is treated as one line 
+*/
+
